faa390298ce815cdfd6f25773514d29d07dba86e
Dane Barentine <dane.barentine@quest.com>
2020-01-30 23:07:02 -0800

    Attempted fix for installing on Polisy

--- install.sh
+++ install.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
+
+# If we are running on Polisy we need to pre-install the py37-zeep package. Otherwise pip will fail.
+unameValue=$(uname -a)
+if [[ $unameValue == *"polisy"* ]]; then
+  echo "Installing py37-zeep"
+  pkg update
+  pkg install -y py37-zeep
+fi;
+
 pip3 install -r requirements.txt --user
