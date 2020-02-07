cd3cb45ba128df7121809783e7fba56c05bbfc36
Dane Barentine <dane.barentine@quest.com>
2020-02-06 21:35:59 -0800

    Remove install for py37-zeep as it's no longer needed

--- install.sh
+++ install.sh
@@ -1,20 +1,5 @@
 #!/usr/bin/env bash
 
 {
-    # If we are running on Polisy we need to pre-install the py37-zeep package. Otherwise pip will fail.
-    platform="$(uname -i)"
-    polisyPlatform="POLISY"
-
-    echo "Current Shell: $SHELL"
-    echo "Platform: $platform"
-
-    if [ "$platform" = "$polisyPlatform" ]; then
-      echo "Installing py37-zeep"
-      sudo /usr/sbin/pkg update
-      sudo /usr/sbin/pkg install -y py37-zeep
-    else
-      echo "Skipping py37-zeep install"
-    fi;
-
     pip3 install -r requirements.txt --user
 } > totalconnect-install.log 2>&1
